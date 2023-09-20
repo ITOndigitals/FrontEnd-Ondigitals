93fb25bd68d65fef68d3d2fabbbe70f8956a697d
Tuấn Lê <n19dccn177@student.ptithcm.edu.vn>
2023-09-20 17:55:36 +0700

    add about-us

--- src/components/aboutuspage/AboutUs.jsx
+++ src/components/aboutuspage/AboutUs.jsx
@@ -1,8 +1,39 @@
+import { useEffect } from "react";
+import IntroAboutUs from "./components/IntroAboutUs/IntroAboutUs";
+import { useBoundStore } from "@/store/useBoundStore";
 
 const AboutUs = () => {
-  return (
-    <div>AboutUs</div>
-  )
-}
+  const setToLight = useBoundStore((state) => state.setToLight);
+  const setHeaderCanChangeColor = useBoundStore(
+    (state) => state.setHeaderCanChangeColor
+  );
+  const setBottomNavIsShown = useBoundStore(
+    (state) => state.setBottomNavIsShown
+  );
+  const setHeaderStickyState = useBoundStore(
+    (state) => state.setHeaderStickyState
+  );
+  const setChangeStickyIsAllowed = useBoundStore(
+    (state) => state.setChangeStickyIsAllowed
+  );
 
-export default AboutUs
+  useEffect(() => {
+    setHeaderStickyState(false);
+    setChangeStickyIsAllowed(true);
+    const header = document.querySelector(".main-header-g");
+    if (header) {
+      setHeaderCanChangeColor();
+      setToLight();
+      setBottomNavIsShown(true);
+      header.classList.remove("hide");
+    }
+  }, []);
+
+  return (
+    <>
+      <IntroAboutUs />
+    </>
+  );
+};
+
+export default AboutUs;
