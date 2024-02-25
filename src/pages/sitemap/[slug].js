f0ef65609953a8219e61fd63f1762a086c8f0a8a
vu nguyen <nguyenduyanhvu2812@gmail.com>
2024-02-26 00:47:46 +0700

    update

--- src/pages/sitemap/[slug].js
+++ src/pages/sitemap/[slug].js
@@ -28,18 +28,14 @@ export async function getServerSideProps({ res, params: { slug } }) {
       notFound: true,
     };
   }
-  let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  let sitemapOds = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
   xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${generateSitemapPaths(pageUrls)}
   </urlset>`;
   res.setHeader("Content-Type", "text/xml; charset=utf-8");
-  res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=600, stale-while-revalidate=600"
-  );
-  res.write(sitemap);
+  res.write(sitemapOds);
   res.end();
   return { props: {} };
 }
--- src/pages/sitemap_index.xml.js
+++ src/pages/sitemap_index.xml.js
@@ -1,30 +1,31 @@
 import getTotalCounts from "../../lib/getTotalCounts";
 import getSitemapPages from "../../utils/getSitemapPages";
 
-const SitemapIndexPage = () => {
+export default function SitemapIndexPage() {
   return null;
-};
+}
 
 export async function getServerSideProps({ res }) {
-  // const details = await getTotalCounts();
-
-  // if (!details || details.length === 0) {
-  //   throw new Error("No data available");
-  // }
-
+  const details = await getTotalCounts();
+  if (!details || details.length === 0) {
+    throw new Error("No data available");
+  }
   const sitemapIndexOds = `<?xml version='1.0' encoding='UTF-8'?>
       <sitemapindex xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
         xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd"
         xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <sitemap>
-          <loc>https://ondigitals.com/sitemapnextjs.xml</loc>
+          <loc>http://localhost:3000/sitemapnextjs.xml</loc>
         </sitemap>
+        ${details.map((item) => getSitemapPages(item)).join("")}
       </sitemapindex>`;
 
   res.setHeader("Content-Type", "text/xml; charset=utf-8");
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=600, stale-while-revalidate=600"
+  );
   res.write(sitemapIndexOds);
   res.end();
-
   return { props: {} };
 }
-export default SitemapIndexPage;
