8b68ee8fa2a9965abf96621ff57675846992cf4c
vu nguyen <nguyenduyanhvu2812@gmail.com>
2024-02-27 16:00:23 +0700

    update 27/02/2024

--- src/pages/sitemap/[slug].js
+++ src/pages/sitemap/[slug].js
@@ -1,11 +1,33 @@
 import getSitemapPageUrls from "../../../lib/getSitemapPageUrls";
-import getTotalCounts from "../../../lib/getTotalCounts";
 import generateSitemapPaths from "../../../utils/generateSitemapPaths";
 
 const SitemapTagPage = () => {
   return null;
 };
 export async function getServerSideProps({ res, params: { slug } }) {
+  const response = await fetch(
+    `https://api.ondigitals.com/wp-json/sitemap/v1/totalpages`
+  );
+
+  if (!response.ok) throw new Error("Failed to fetch data");
+
+  const data = await response.json();
+
+  if (!data) throw new Error("No data available");
+
+  const excludeItems = [
+    "user",
+    "card_review",
+    "blogs_content",
+    "case_study",
+    "client",
+    "tag",
+    "category",
+  ];
+
+  const totalArray = Object.keys(data)
+    .filter((name) => !excludeItems.includes(name))
+    .map((name) => ({ name, total: data[name] }));
   let isXml = slug.endsWith(".xml");
   if (!isXml) {
     return {
@@ -28,7 +50,7 @@ export async function getServerSideProps({ res, params: { slug } }) {
   let type = slugArray[0];
   let pageNo = slugArray[1]?.match(/(\d+)/)[0] ?? null;
   let page = pageNo ? parseInt(pageNo) : null;
-  let possibleTypes = await getTotalCounts();
+  let possibleTypes = totalArray;
   if (!possibleTypes.some((e) => e.name === type)) {
     return {
       notFound: true,
