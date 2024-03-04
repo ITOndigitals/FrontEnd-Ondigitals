c83d450863e7524dc842855823b940c76bc1ae63
vu nguyen <nguyenduyanhvu2812@gmail.com>
2024-03-04 16:26:06 +0700

    update 301 list post vietnam 04/03/2024

--- src/middleware.js
+++ src/middleware.js
@@ -3,14 +3,14 @@ import { dataSlugPostVi } from "../utils/dataSlugPostVi";
 
 export function middleware(request) {
   const targetHost = "ondigitals.com";
+  // const host = request.headers.get("host");
+  // const protocol = request.headers.get("x-forwarded-proto") || "http"; // Sử dụng header "x-forwarded-proto" để xác định giao thức nếu bạn đang sử dụng proxy
+  // // Kiểm tra nếu giao thức không phải HTTPS hoặc host có tiền tố www
+  // if (protocol !== "https" || host.startsWith("www.")) {
+  //   const redirectUrl = `https://${targetHost}${request.nextUrl.pathname}`;
+  //   return NextResponse.redirect(redirectUrl, { status: 301 });
+  // }
   const urlMain = "https://ondigitals.com";
-  const host = request.headers.get("host");
-  const protocol = request.headers.get("x-forwarded-proto") || "http"; // Sử dụng header "x-forwarded-proto" để xác định giao thức nếu bạn đang sử dụng proxy
-  // Kiểm tra nếu giao thức không phải HTTPS hoặc host có tiền tố www
-  if (protocol !== "https" || host.startsWith("www.")) {
-    const redirectUrl = `https://${targetHost}${request.nextUrl.pathname}`;
-    return NextResponse.redirect(redirectUrl, { status: 301 });
-  }
   const modifiedData = dataSlugPostVi.map((item) => ({
     slug: `${urlMain}/${item.slug}/`,
     slugNewVi: `${urlMain}/vi/${item.slug}/`,
@@ -24,7 +24,6 @@ export function middleware(request) {
     // Thực hiện chuyển hướng đến slugNewVi của phần tử đó
     return NextResponse.redirect(matchedItem.slugNewVi, {
       status: 301,
-      method: request.method,
     });
   }
   return NextResponse.next();
