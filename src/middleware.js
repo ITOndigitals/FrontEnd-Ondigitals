63c88970f09d81065f708a3cd63ce06cf067ba6a
vu nguyen <nguyenduyanhvu2812@gmail.com>
2024-12-17 22:56:20 +0700

    update

--- src/middleware.js
+++ src/middleware.js
@@ -13,10 +13,10 @@ export function middleware(request) {
   const host = request.headers.get("host");
   const protocol = request.headers.get("x-forwarded-proto") || "http"; // Sử dụng header "x-forwarded-proto" để xác định giao thức nếu bạn đang sử dụng proxy
   // Kiểm tra nếu giao thức không phải HTTPS hoặc host có tiền tố www
-  // if (protocol !== "https" || host.startsWith("www.")) {
-  //   const redirectUrl = `https://${targetHost}${request.nextUrl.pathname}`;
-  //   return NextResponse.redirect(redirectUrl, { status: 301 });
-  // }
+  if (protocol !== "https" || host.startsWith("www.")) {
+    const redirectUrl = `https://${targetHost}${request.nextUrl.pathname}`;
+    return NextResponse.redirect(redirectUrl, { status: 301 });
+  }
   // // // Kiểm tra nếu cookie "hasRedirected" đã tồn tại
   // const cookie = request.cookies.get("hasRedirected");
   // if (!cookie) {
